bb6af42f1d3aa526cfedee577b556c74b61f55fd
miartayasa <miartayasa10@gmail.com>
2023-05-17 12:35:09 +0800

    first commit v1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,7 @@
 <footer class="c-footer">
     <div>{!! settings()->footer_text !!}</div>
 
-    <div class="mfs-auto d-md-down-none">Powered by&nbsp;<a href="https://laravel.com" target="_blank">Laravel</a></div>
+    <div class="mfs-auto d-md-down-none">Powered by&nbsp;<a href="https://www.linkedin.com/in/igede-miarta"
+            target="_blank">IGede Miarta</a>
+    </div>
 </footer>
